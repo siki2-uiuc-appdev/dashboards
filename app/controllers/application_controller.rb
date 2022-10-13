1aa7d46c9a773cae6d939ae6fbf98ea7e710a8be
siki2-uiuc <siki2@illinois.edu>
2022-10-13 16:09:52 +0000

    routes complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  
+  def homepage
+    render({ :template => "dashboard_templates/homepage.html.erb" })
+  end
+
+  def exchange_rates
+    render({ :template => "dashboard_templates/exchange_rates.html.erb"})
+  end
 end
